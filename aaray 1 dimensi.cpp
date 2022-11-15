4fea0addb0474b56210ce2bc15db885a0239f2e9
ReyhanPt <118263106+ReyhanPt@users.noreply.github.com>
2022-11-15 17:40:48 +0700

    Add files via upload

---
+++ aaray 1 dimensi.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+ int arrayDimensi1[5];
+
+ cout<<"===================================\n";
+ cout<<"Nama : REYHAN PUTRA FEBRIAN\n";
+ cout<<"NIM : 22343069\n";
+ cout<<"Program Membuat Array 1 Dimensi\n";
+ cout<<"===================================\n\n";
+ cout<<"Masukkan Data Ke-1 : ";
+ cin>>arrayDimensi1[0];
+ cout<<"Masukkan Data Ke-2 : ";
+ cin>>arrayDimensi1[1];
+ cout<<"Masukkan Data Ke-3 : ";
+ cin>>arrayDimensi1[2];
+ cout<<"Masukkan Data Ke-4 : ";
+ cin>>arrayDimensi1[3];
+ cout<<"Masukkan Data Ke-5 : ";
+ cin>>arrayDimensi1[4];
+
+ cout<<endl;
+
+ cout<<"Data Ke-1 : "<<arrayDimensi1[0]<<endl;
+ cout<<"Data Ke-2 : "<<arrayDimensi1[1]<<endl;
+ cout<<"Data Ke-3 : "<<arrayDimensi1[2]<<endl;
+ cout<<"Data Ke-4 : "<<arrayDimensi1[3]<<endl;
+ cout<<"Data Ke-5 : "<<arrayDimensi1[4]<<endl;
+
+}
